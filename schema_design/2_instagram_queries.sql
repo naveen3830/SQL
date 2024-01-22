0fbc88fc9e2c49e5d0b8723f1a200d7b3b24fcba
naveen3830 <naveenr3830@gmail.com>
2024-01-22 15:53:28 +0530

    Added changes

--- schema_design/2_instagram_queries.sql
+++ schema_design/2_instagram_queries.sql
@@ -8,7 +8,9 @@ SELECT *From follows;
 SELECT *FROM tags;
 SELECT *FROM photo_tags;
 
+-- TO find 5 oldest users
 SELECT username,created_at from users ORDER BY users.created_at DESC LIMIT 5;
+-- To get the days on which most users registered
 SELECT DAYNAME(users.created_at) AS day,count(*) AS total from users GROUP BY day ORDER BY total DESC;
 SELECT DAYNAME(users.created_at) AS day,count(*) AS total from users GROUP BY day ORDER BY total DESC LIMIT 2;
 
@@ -23,8 +25,7 @@ INNER JOIN users ON photos.user_id=users.id
 GROUP BY photos.id ORDER BY most_likes DESC LIMIT 1;
 
 -- avg number of photos per users
-SELECT
-    (SELECT COUNT(*) FROM photos)/(SELECT COUNT(*) FROM users) AS avg_posts;
+SELECT(SELECT COUNT(*) FROM photos)/(SELECT COUNT(*) FROM users) AS avg_posts;
 
 SELECT tag_name,count(*) AS total from tags
 INNER join photo_tags on photo_tags.tag_id=tags.id GROUP BY tag_name ORDER BY total DESC LIMIT 5;
